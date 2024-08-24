1ac05fda9fd071071c09913e90c3ac7585de329e
Arvin Nemati <arvinnemati95@gmail.com>
2024-08-24 18:49:07 +0330

    added a better design and feel to the site

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+//Fixed header
 
-console.log('hey')
+window.addEventListener('scroll', function() {
+    var fixedHeader = document.getElementById('fixed-header');
+    var triggerHeight = 100; // Adjust this value to set when the fixed header appears
+  
+    if (window.scrollY > triggerHeight) {
+      fixedHeader.classList.remove('hidden');
+    } else {
+      fixedHeader.classList.add('hidden');
+    }
+  });
+
+
+
+//Books
 
 document.querySelector('button').addEventListener('click',getFetch)
 
@@ -28,6 +42,9 @@ function getFetch(){
 
             //for each iteration create a section with relevant elements inside
             let section = document.createElement('section')
+
+            //add class to section to style with flex
+            section.classList.add('section-container')
             //h3 for TITLE
             let h3 = document.createElement('h3')
             h3.textContent = result.title
@@ -41,7 +58,9 @@ function getFetch(){
                 li.textContent = subject
                 ul.appendChild(li)
             })
-
+            //h3 element for Writer
+            let writerTitle = document.createElement('h3')
+            writerTitle.textContent = 'Writer(s):'
             //ul element for Authors
 
             let ulAuthors = document.createElement('ul')
@@ -64,6 +83,7 @@ function getFetch(){
             section.appendChild(img)
             section.appendChild(h3)
             section.appendChild(ul)
+            section.appendChild(writerTitle)
             section.appendChild(ulAuthors)
             section.appendChild(download)
             document.querySelector('#container').appendChild(section)
